ea592c4f52198fbaffb59f7efb4dda5a5ff8b33b
atsegelnyk <tonyarthome@gmail.com>
2022-08-27 23:39:49 +0300

    done

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 initalPricesArr = [1.03, 135.85, 1.2, 1.3, 0.68, 0.62, 0.86, 1.51]
+ufpExpression = "psrand_arr[count] % 10"
 
 ID = 0
 CREATIONDATE = 1
@@ -11,3 +12,52 @@ FILLVOLUME = 7
 INITALPRICE = 8
 FILLPRICE = 9
 update_range = 3
+
+ipgFullAmount = 100
+ipgAmount0 = 0
+ipgAmount1 = 12
+ipgAmount2 = 24
+ipgAmount3 = 36
+ipgAmount4 = 48
+ipgAmount5 = 60
+ipgAmount6 = 72
+ipgAmount7 = 84
+
+usVal = 3
+usVal0 = 0
+usVal1 = 1
+
+cdPart = 10
+cdAdd = 5
+
+ivgStep = 0.1
+
+dgVal = 3
+dgEqVal = 0
+dgValF = "buy"
+dgValT = "sell"
+
+pgVal = 10
+pgEqVal0 = 1
+pgEqVal1 = 3
+pgEqVal2 = 5
+
+gcoState = "new"
+gcoFillPrice = 0
+
+state1 = "new"
+state2 = "inProcess"
+state3 = "fill"
+state4 = "partialFill"
+state5 = "cancelled"
+state6 = "done"
+
+ufvAmount = 4
+ufvValue = 1
+
+randPercent1 = 1
+randPercent2 = 2
+randPercent3 = 3
+randPercent4 = 4
+randPercent5 = 5
+
--- function.py
+++ function.py
@@ -2,24 +2,10 @@ from datetime import datetime
 from setup import *
 from constants import *
 
+
 def percentage(percent, number):
-  percentage = number/100*percent
-  return percentage
-
-
-def condition(todo, expression=None, eq=None, expected=None):
-    if expression == 0:
-        exec(todo)
-    else:
-        if eq == 'equals':
-            if eval(expression) == eval(expected):
-                exec(todo)
-        elif eq == 'less':
-            if eval(expression) <= eval(expected):
-                exec(todo)
-        elif eq == "greater":
-            if eval(expression) >= eval(expected):
-                exec(todo)
+    percentage = number / 100 * percent
+    return percentage
 
 
 def psrand(alpha, module, step, number):
@@ -31,17 +17,18 @@ def psrand(alpha, module, step, number):
         previous = current
     return data_arr
 
-def inital_volumes_generate(alpha, module, step, number):
+
+def initial_volumes_generate(alpha, module, step, number):
     previous = 0
     data_arr2 = []
     for i in range(number):
-        current = ((alpha * previous + step) % module) + 0.1
+        current = ((alpha * previous + step) % module) + ivgStep
         data_arr2.append(round(current, 1))
         previous = current
     return data_arr2
 
 data_arr = psrand(alpha, module, step, order_range)
-data_arr2 = inital_volumes_generate(volumeAlpha, volumeModule, volumeStep, order_range)
+data_arr2 = initial_volumes_generate(volumeAlpha, volumeModule, volumeStep, order_range)
 
 
 def id_generate(data_arr):
@@ -64,55 +51,56 @@ def date_generate(data_arr):
 def direction_generate(data_arr):
     direction_arr = []
     for i in range(order_range):
-        temp = (data_arr[i] % 3)
-        if (temp == 0):
-            direction_arr.append("sell")
+        temp = (data_arr[i] % dgVal)
+        if temp == dgEqVal:
+            direction_arr.append(dgValT)
         else:
-            direction_arr.append("buy")
+            direction_arr.append(dgValF)
     return direction_arr
 
 
 def pair_generate(data_arr):
     pair_arr = []
     for i in range(order_range):
-        temp = (data_arr[i] % 10)
-        if temp == 1:
+        temp = (data_arr[i] % pgVal)
+        if temp == pgEqVal0:
             pair_arr.append("EURUSD")
-        elif temp == 3:
+        elif temp == pgEqVal1:
             pair_arr.append("USDJPY")
-        elif temp == 5:
+        elif temp == pgEqVal2:
             pair_arr.append("GBPUSD")
         else:
             pair_arr.append("USDCHF")
     return pair_arr
 
 
-def inital_price_generate(data_arr):
+def initial_price_generate(data_arr):
     prices = []
     for i in range(order_range):
-        if data_arr[i] % 100 > 0 and data_arr[i] % 100 <= 12:
+        if ipgAmount0 < data_arr[i] % ipgFullAmount <= ipgAmount1:
             prices.append(initalPricesArr[0])
-        elif data_arr[i] % 100 > 12 and data_arr[i] % 100 <= 24:
+        elif ipgAmount1 < data_arr[i] % ipgFullAmount <= ipgAmount2:
             prices.append(initalPricesArr[1])
-        elif data_arr[i] % 100 > 24 and data_arr[i] % 100 <= 36:
+        elif ipgAmount2 < data_arr[i] % ipgFullAmount <= ipgAmount3:
             prices.append(initalPricesArr[2])
-        elif data_arr[i] % 100 > 36 and data_arr[i] % 100 <= 48:
+        elif ipgAmount3 < data_arr[i] % ipgFullAmount <= ipgAmount4:
             prices.append(initalPricesArr[3])
-        elif data_arr[i] % 100 > 48 and data_arr[i] % 100 <= 60:
+        elif ipgAmount4 < data_arr[i] % ipgFullAmount <= ipgAmount5:
             prices.append(initalPricesArr[4])
-        elif data_arr[i] % 100 > 60 and data_arr[i] % 100 <= 72:
+        elif ipgAmount5 < data_arr[i] % ipgFullAmount <= ipgAmount6:
             prices.append(initalPricesArr[5])
-        elif data_arr[i] % 100 > 72 and data_arr[i] % 100 <= 84:
+        elif ipgAmount6 < data_arr[i] % ipgFullAmount <= ipgAmount7:
             prices.append(initalPricesArr[6])
         else:
             prices.append(initalPricesArr[7])
 
     return prices
 
+
 def fill_prices_generate(data_arr, initalPrice, direction):
     fillPricesArr = []
     for i in range(order_range):
-        if direction == "buy":
+        if direction == dgValF:
             fillPricesArr.append(round(float(initalPrice[i]) - (float(initalPrice[i]) / float(data_arr[i])), 2))
         else:
             fillPricesArr.append(round(float(initalPrice[i]) + (float(initalPrice[i]) / float(data_arr[i])), 2))
@@ -124,33 +112,31 @@ def generateCurrentOrder():
     ids = id_generate(data_arr)
     dates = date_generate(data_arr)
     changeDates = date_generate(data_arr)
-    states = []
     directions = direction_generate(data_arr)
     pairs = pair_generate(data_arr)
-    initalVolumes = data_arr2
-    fillVolumes = []
-    initalPrices = inital_price_generate(data_arr)
-    fillPrices = fill_prices_generate(data_arr2, initalPrices, directions)
+    initialVolumes = data_arr2
+    initialPrices = initial_price_generate(data_arr)
+    fillPrices = fill_prices_generate(data_arr2, initialPrices, directions)
     currentOrder = []
     for row in range(order_range):
         currentOrder.append([
-                            ids[row],
-                            dates[row],
-                            changeDates[row],
-                            "new",
-                            directions[row],
-                            pairs[row],
-                            initalVolumes[row],
-                            0,
-                            initalPrices[row],
-                            fillPrices[row]
-                            ])
+            ids[row],
+            dates[row],
+            changeDates[row],
+            gcoState,
+            directions[row],
+            pairs[row],
+            initialVolumes[row],
+            gcoFillPrice,
+            initialPrices[row],
+            fillPrices[row]
+        ])
 
     return currentOrder
 
 
 def change_date_generate(date, count):
-    diff = data_arr[count]/10 + 1
+    diff = data_arr[count] / cdPart + cdAdd
     dateTimestamp = datetime.strptime(date, '%Y-%m-%d %H:%M:%S').timestamp()
     if dateTimestamp + diff in range(block1, block1 + timestampDiff):
         changedDate = str(datetime.fromtimestamp(dateTimestamp + diff + timestampDiff).replace(microsecond=0))
@@ -164,54 +150,63 @@ def change_date_generate(date, count):
 
 
 def update_state(state, psrand_arr, count):
-    if state == "new":
-        updState = "inProcess"
-
-    elif state == "inProcess":
-        if psrand_arr[count] % 3 == 0:
-            updState = "partialFill"
-        elif psrand_arr[count] % 3 == 1:
-            updState = "fill"
+    if state == state1:
+        updState = state2
+    elif state == state2:
+        if psrand_arr[count] % usVal == usVal0:
+            updState = state4
+        elif psrand_arr[count] % usVal == usVal1:
+            updState = state3
         else:
-            updState = "cancelled"
-
-    else: #state == "fill" or state == "partialFill" or state == "cancelled":
-        updState = "done"
+            updState = state5
+    else:
+        updState = state6
 
     return updState
 
 
 def update_fill_volumes(initalVolume, state, psrand_arr, count):
-    if state == "new" or state == "cancelled" or state == "inProcess":
+    if state == state1 or state == state5 or state == state2:
         updFillVolume = 0
-    elif state == "fill" or state == "done":
+    elif state == state3 or state == state6:
         updFillVolume = initalVolume
     else:
-        if psrand_arr[count] % 4 == 1:
-            updFillVolume = initalVolume + 0.1
+        if psrand_arr[count] % ufvAmount == ufvValue:
+            updFillVolume = initalVolume + ivgStep
         else:
-            updFillVolume = initalVolume - 0.1
+            updFillVolume = initalVolume - ivgStep
     return round(updFillVolume, 2)
 
 
 def update_fill_price(initalPrice, direction, psrand_arr, count):
-    expression = eval('psrand_arr[count] % 10')
-    if direction == "buy":
+    global updFillPrice
+    expression = eval(ufpExpression)
+    if direction == dgValF:
 
         match expression:
-            case 1: updFillPrice = initalPrice - percentage(1, initalPrice)
-            case 3: updFillPrice = initalPrice - percentage(2, initalPrice)
-            case 5: updFillPrice = initalPrice - percentage(3, initalPrice)
-            case 7: updFillPrice = initalPrice - percentage(4, initalPrice)
-            case 9: updFillPrice = initalPrice - percentage(5, initalPrice)
+            case 1:
+                updFillPrice = initalPrice - percentage(randPercent1, initalPrice)
+            case 3:
+                updFillPrice = initalPrice - percentage(randPercent2, initalPrice)
+            case 5:
+                updFillPrice = initalPrice - percentage(randPercent3, initalPrice)
+            case 7:
+                updFillPrice = initalPrice - percentage(randPercent4, initalPrice)
+            case 9:
+                updFillPrice = initalPrice - percentage(randPercent5, initalPrice)
 
     else:
         match expression:
-            case 1: updFillPrice = initalPrice + percentage(1, initalPrice)
-            case 3: updFillPrice = initalPrice + percentage(2, initalPrice)
-            case 5: updFillPrice = initalPrice + percentage(3, initalPrice)
-            case 7: updFillPrice = initalPrice + percentage(4, initalPrice)
-            case 9: updFillPrice = initalPrice + percentage(5, initalPrice)
+            case 1:
+                updFillPrice = initalPrice + percentage(randPercent1, initalPrice)
+            case 3:
+                updFillPrice = initalPrice + percentage(randPercent2, initalPrice)
+            case 5:
+                updFillPrice = initalPrice + percentage(randPercent3, initalPrice)
+            case 7:
+                updFillPrice = initalPrice + percentage(randPercent4, initalPrice)
+            case 9:
+                updFillPrice = initalPrice + percentage(randPercent5, initalPrice)
 
     return round(updFillPrice, 3)
 
@@ -222,20 +217,22 @@ def generateOrderHistory(currentOrderList):
     for order in currentOrderList:
         orderHistory.append(order)
 
-
     for update in range(update_range):
         for order in range(order_range):
             tempCurrentOrderList.append([currentOrderList[order][ID],
-                                 currentOrderList[order][CREATIONDATE],
-                                 change_date_generate(currentOrderList[order][CHANGEDATE], order),
-                                 update_state(currentOrderList[order][STATE], data_arr, order),
-                                 currentOrderList[order][INSTRUMENT],
-                                 currentOrderList[order][DIRECTION],
-                                 currentOrderList[order][INITALPRICE],
-                                 update_fill_volumes(currentOrderList[order][INITALVOLUME], update_state(currentOrderList[order][STATE], data_arr, order), data_arr, order),
-                                 currentOrderList[order][INITALPRICE],
-                                 update_fill_price(currentOrderList[order][INITALPRICE], currentOrderList[order][STATE], data_arr, order)
-                                 ])
+                currentOrderList[order][CREATIONDATE],
+                                         change_date_generate(currentOrderList[order][CHANGEDATE], order),
+                                         update_state(currentOrderList[order][STATE], data_arr, order),
+                                         currentOrderList[order][INSTRUMENT],
+                                         currentOrderList[order][DIRECTION],
+                                         currentOrderList[order][INITALPRICE],
+                                         update_fill_volumes(currentOrderList[order][INITALVOLUME],
+                                                             update_state(currentOrderList[order][STATE], data_arr,
+                                                                          order), data_arr, order),
+                                         currentOrderList[order][INITALPRICE],
+                                         update_fill_price(currentOrderList[order][INITALPRICE],
+                                                           currentOrderList[order][STATE], data_arr, order)
+                                         ])
         for order in tempCurrentOrderList:
             orderHistory.append(order)
         currentOrderList = []
@@ -244,9 +241,3 @@ def generateOrderHistory(currentOrderList):
         tempCurrentOrderList = []
 
     return orderHistory
-
-
-
-
-
-
